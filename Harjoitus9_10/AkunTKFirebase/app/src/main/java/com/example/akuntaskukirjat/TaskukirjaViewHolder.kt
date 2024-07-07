980729f60de9519694fdd999f65e3642b283a81c
Markus Siitonen <markus.siitonen@tuni.fi>
2024-07-07 15:00:17 +0300

    Harjoitukset 9 ja 10 valmiina. Selkeytetty kansiorakennetta

---
+++ Harjoitus9_10/AkunTKFirebase/app/src/main/java/com/example/akuntaskukirjat/TaskukirjaViewHolder.kt
@@ -0,0 +1,34 @@
+package com.example.akuntaskukirjat
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class TaskukirjaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private var taskukirjaNroItemView : TextView = itemView.findViewById(R.id.textViewNro)
+    private var taskukirjaNimiItemView : TextView = itemView.findViewById(R.id.textViewnimi)
+
+    private var delButton : Button = itemView.findViewById(R.id.buttonDelete)
+
+    fun bind(taskukirja: Taskukirja) {
+        taskukirjaNroItemView.text = taskukirja.number.toString()
+        taskukirjaNimiItemView.text = taskukirja.name
+    }
+
+    companion object {
+        fun create(parent: ViewGroup): TaskukirjaViewHolder {
+            val view: View = LayoutInflater.from(parent.context)
+                .inflate(R.layout.recyclerview_item, parent, false)
+            return TaskukirjaViewHolder(view)
+        }
+    }
+
+    fun getDelButton(): Button {
+        return this.delButton
+    }
+
+}
