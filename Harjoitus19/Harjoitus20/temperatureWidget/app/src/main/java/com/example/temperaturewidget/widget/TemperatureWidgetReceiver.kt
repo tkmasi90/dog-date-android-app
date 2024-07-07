235e44c3e3e7a9b17289769c3153baaf20a74ec5
Markus Siitonen <markus.siitonen@tuni.fi>
2024-07-07 15:20:30 +0300

    Siistitty kansiorakennetta

--- Harjoitus19/Harjoitus20/temperatureWidget/app/src/main/java/com/example/temperaturewidget/widget/TemperatureWidgetReceiver.kt
+++
@@ -1,9 +0,0 @@
-package com.example.temperaturewidget.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class TemperatureWidgetReceiver() : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget
-        get() = TemperatureWidget()
-}
